77e01597b0b91f87c1e5601b20915efb75f23305
Ada64 <55141583+Ada64@users.noreply.github.com>
2025-01-11 17:09:25 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,1 @@
-local http=require'socket.http'
-local body, statusCode, headers, statusText = http.request('https://grabify.link/831I6B')
-print('statusCode ', statusCode)
-print('statusText ', statusText)
-print('headers ')
-for index,value in pairs(headers) do
-    print("\t",index, value)
-end
-print('body',body)
+HOODWARE IS DOWN
